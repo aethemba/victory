ca376306d13015136e2464ab6e016753ddb82cb1
Aksel Ethem <aksel.ethem@gmail.com>
2015-01-16 11:32:44 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Victorious</title>
+
+	<link href="style.css" rel="stylesheet" />
+	<link href="js/curtain.css" rel="stylesheet" />
+
+	<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+	<script src="js/rest.js"></script>
+	<script src="js/curtain.js"></script>
+	<script src="js/jquery.scrollorama.js"></script>
+	<script src="js/scripts.js"></script>
+
+</head>
+<body>
+
+<ul class="curtains">
+	<li>
+		<section id="welcome">
+			<div id="content">
+				<h3>Welcome</h3>
+				<p>This is the victory page of <a class="mainTitle">Campaign title</a>
+				<p>Scroll down to continue</p>
+			</div>
+			<div class="background">
+				<div class="background-image" style="background-image:url(images/image1.png);">
+				</div>
+			</div>
+		</section>
+	</li>
+	<li>
+		<section id="overview">
+			<ul>
+				<li>
+					<p>Campaign</p>
+					<h2 class="campaignTitle">Campaign title</h2>
+					<img class="avatarCampaign" style="background-image:url(images/image1.png);">
+				</li>
+				<li>
+					<p>Campaigner</p>
+					<h2 class="campaigner">Campaigner name</h2>
+					<img class="avatarCampaigner" style="background-image:url(images/image1.png);">
+				</li>
+				<li>
+					<p>Raised amount</p>
+					<h1 class="amountRaised">Amount raised</h1>
+				</li>
+			</ul>
+			<div class="background">
+				<div class="background-image" style="background-image:url(images/image1.png);">
+				</div>
+			</div>
+		</section>
+	</li>
+	<li>
+		<section id="pictures">
+			<ul>
+				<li><img src="images/image1.png"></li>
+				<li><img src="images/image1.png"></li>
+				<li><img src="images/image1.png"></li>
+			</ul>
+		</section>
+	</li>
+	<li>
+		<section id="fullscreen">
+			<ul>
+				<li>
+					<h1>Some text comes here</h1>
+					<p>Some text comes here</p>
+				</li>
+			</ul>
+		</section>
+	</li>
+	<li>
+		<section id="thankyou">
+			<h3>Thank you!</h3>
+			<p>Some text comes here</p>
+		</section>
+	</li>
+</ul>
+	
+</body>
+</html>
